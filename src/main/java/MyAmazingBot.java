bde4de9afcb91daaf4fd8266bb4f41f0c72d36af
PosokhovOleksii <72158429+PosokhovOleksii@users.noreply.github.com>
2020-11-29 15:26:51 +0200

    add keyboard

--- src/main/java/MyAmazingBot.java
+++ src/main/java/MyAmazingBot.java
@@ -1,7 +1,12 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyAmazingBot extends TelegramLongPollingBot {
     public String getBotUsername() {
@@ -24,6 +29,27 @@ public class MyAmazingBot extends TelegramLongPollingBot {
             SendMessage message = new SendMessage() // Create a message object object
                     .setChatId(chat_id)
                     .setText(message_text);
+
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            List <KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+            KeyboardRow row = new KeyboardRow();
+
+            row.add("Row 1 Button 1");
+            row.add("Row 2 Button 2");
+            row.add("Row 3 Button 3");
+
+            keyboard.add(row);
+
+            row = new KeyboardRow();
+
+            row.add("Row 2 Button 1");
+            row.add("Row 2 Button 2");
+            row.add("Row 3 Butoon 3");
+
+            keyboard.add(row);
+
+            keyboardMarkup.setKeyboard(keyboard);
+            message.setReplyMarkup(keyboardMarkup);
             try {
                 execute(message); // Sending our message object to user
             } catch (TelegramApiException e) {
